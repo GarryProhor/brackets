7524fc4d07f13890d5c48e2b0a2b01f5130360b2
igorp <igorprohorchenko@gmail.com>
2023-08-08 09:49:38 +0300

    task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+    bracketsConfig = bracketsConfig.map(item => item.join(''));
+
+    while (bracketsConfig.length) {
+        let iterator = 0;
+        bracketsConfig.forEach(item => {
+            if (str.indexOf(item) !== -1) {
+                str = str.replace(item, '');
+            } else {
+                iterator += 1;
+            }
+        });
+        if (iterator === bracketsConfig.length) bracketsConfig.length = 0;
+    }
+    return str.length === 0;
+};
